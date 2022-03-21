62980a1968f7a9db6cfa36f2ce64c30552204db1
Samuele Barzaghi <sbarzaghi@alteanet.it>
2022-03-21 11:56:37 +0100

    Doc

---
+++ README.md
@@ -0,0 +1,10 @@
+# Esempio utilizzo moduli nodejs asincroni con xsjs
+
+## Per provare:
+
+- Clonare il repository in locale
+- `npm start`
+- esempio utilizzo modulo `request`:
+  - http://localhost:3000/test-request-sync.xsjs
+- esempio utilizzo modulo custom locale
+  - http://localhost:3000/test-nodejs-sync.xsjs
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-'use strict';
+"use strict";
 
-var xsenv = require('@sap/xsenv');
-var xsjs = require('@sap/xsjs');
+var xsenv = require("@sap/xsenv");
+var xsjs = require("@sap/xsjs");
 
 var port = process.env.PORT || 3000;
-var options = {anonymous: true};
+var options = { anonymous: true };
 
 xsenv.loadEnv();
 
 xsjs(options).listen(port);
 
-console.log('Node XS server listening on port %d', port);
+console.log("Node XS server listening on port %d", port);
+console.log()
+console.log(`Esempio chiamata asincrona in modulo npm:`);
+console.log(`http://localhost:${port}/test-request-sync.xsjs`);
+console.log()
+console.log(`Esempio chiamata asincrona in modulo custom locale:`);
+console.log(`http://localhost:${port}/test-nodejs-sync.xsjs`);
